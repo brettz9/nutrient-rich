2d8a083d608c21b0bc22ea85b42302e08a1a2907
Brett Zamir <brettz9@yahoo.com>
2021-04-10 15:36:39 +0800

    - Remove persistent storage requirement for now

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import /* NutrientRichApp from */ './components/NutrientRichApp.js';
 
 // const baseURL = 'https://api.nal.usda.gov/fdc/v1/';
 
-(async () => {
+(() => {
 if (!navigator.storage || !navigator.storage.persist) {
   HyperHTMLElement.bind($('body'))`
     <b>Your browser does not support persistent storage.
@@ -21,16 +21,18 @@ if (!navigator.storage || !navigator.storage.persist) {
 HyperHTMLElement.bind($('body'))`
   <b>Requesting permissions...</b>
 `;
-const persistent = await navigator.storage.persist();
-if (persistent) {
-  HyperHTMLElement.bind($('body'))`
-    <nutrient-rich-app />
-  `;
-  // api-base-url="${baseURL}"
+// const persistent = await navigator.storage.persist();
+// if (persistent) {
+HyperHTMLElement.bind($('body'))`
+  <nutrient-rich-app />
+`;
+// api-base-url="${baseURL}"
+/*
 } else {
   HyperHTMLElement.bind($('body'))`
     <b>You must approve permissions to use this application.
         Refresh to try again.</b>
   `;
 }
+*/
 })();
